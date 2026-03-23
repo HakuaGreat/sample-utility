4c1642f6bf8fdc1fb67388fbe9f37269f4d2cd01
HakuaGreat <64060315+HakuaGreat@users.noreply.github.com>
2026-03-23 10:24:11 +0900

    CsvLineParser.java の作成

---
+++ CsvLineParser.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvLineParser {
+
+    public List<String> parse(String line) {
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        boolean inQuotes = false;
+
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+
+            if (c == '"') {
+                if (inQuotes && i + 1 < line.length() && line.charAt(i + 1) == '"') {
+                    sb.append('"');
+                    i++;
+                } else {
+                    inQuotes = !inQuotes;
+                }
+            } else if (c == ',' && !inQuotes) {
+                result.add(sb.toString());
+                sb.setLength(0);
+            } else {
+                sb.append(c);
+            }
+        }
+
+        result.add(sb.toString());
+        return result;
+    }
+}
